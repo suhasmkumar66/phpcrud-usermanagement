e19def44a39cdff4176229ad1eadc6de31f6aeed
Suhas M <suhasmkumar66@gmail.com>
2021-05-07 21:39:16 +0530

    db changes

--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "root", "test","3306");
+$con=mysqli_connect("sql6.freemysqlhosting.net", "sql6410958", "HS8uC6DQ2U", "sql6410958","3306");
 if(mysqli_connect_errno())
 {
 echo "Connection Fail".mysqli_connect_error();
